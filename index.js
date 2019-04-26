3dd90ccc1bc600154a0aeeee53d7f1520bf2ceab
Damir-Che <chechuhov.damir@maikl.ru>
2019-04-26 16:49:30 +0300

    Added fancybox

--- index.js
+++ index.js
@@ -23,6 +23,17 @@ $(document).ready(function(){
 		$('.content').toggleClass('cont-active');
 	})
 
+	// картинки
+	// $("a.gallery").fancybox();
+
+	// 	$("a.gallery").fancybox({
+	// 		"padding" : 20, // отступ контента от краев окна
+	// 		"zoomSpeedIn" : 1000,	// скорость анимации в мс при увеличении фото (по умолчанию 0)
+	// 		"zoomSpeedOut" : 1000,	// скорость анимации в мс при уменьшении фото (по умолчанию 0)
+	// 		"overlayOpacity" : 0.5,	 // Прозрачность затенения 	(0.3 по умолчанию)
+	// 		"centerOnScroll" : false // Если TRUE окно центрируется на экране, когда 
+	// 	})
+
 
 })
 
